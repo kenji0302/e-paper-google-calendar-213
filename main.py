bfb14b1af0670c40fad551c7a4db33169cad1f87
kenji0302 <kenji0302@gmail.com>
2024-07-21 15:23:49 +0900

    認証失敗時の表示を追加

--- main.py
+++ main.py
@@ -23,53 +23,69 @@ if __name__=='__main__':
         # カレンダー取得
         access_token = refresh_access_token(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REFRESH_TOKEN)
 
-        calendar_id = "primary"
-
-        ymdhms = jst_today_ymdhms_for_api() 
-        
-        url = "https://www.googleapis.com/calendar/v3/calendars/" + GOOGLE_CALENDAR_ID + "/events?maxResults=6&timeMin=" + ymdhms + "&orderBy=startTime&singleEvents=True&timeZone=JST"
-
-        headers = {
-            "Authorization": "Bearer " + access_token
-        }
-
-        response = urequests.get(url, headers=headers)
-
-        if response.status_code == 200:
-
-            events = response.json().get('items', [])            
-            
-            # 画面表示
-            epd = EPD_2in13_B_V4_Landscape()
-            epd.Clear(0xff, 0xff)
-            epd.imageblack.fill(0xff)
-            epd.imagered.fill(0xff)
+        # 画面表示
+        epd = EPD_2in13_B_V4_Landscape()
+        epd.Clear(0xff, 0xff)
+        epd.imageblack.fill(0xff)
+        epd.imagered.fill(0xff)
 
 
-            mf = mfont()
-            mf.setFontSize(16)    
+        mf = mfont()
+        mf.setFontSize(16)    
 
-            x = 10
-            i = 0
-            
-            for event in events:
-                if 'date' in event['start']:
-                    event_ymd = event['start']['date'][0:4] + event['start']['date'][5:7] + event['start']['date'][8:10]
-                    event_str = event['start']['date'][5:7] + '月' + event['start']['date'][8:10] + '日' + ' ' + event['summary']
-                    print(event_str)
-                elif 'dateTime' in event['start']:
-                    event_ymd = event['start']['dateTime'][0:4] + event['start']['dateTime'][5:7] + event['start']['dateTime'][8:10]
-                    event_str = event['start']['dateTime'][5:7] + '月' + event['start']['dateTime'][8:10] + '日' +   event['start']['dateTime'][11:16] + ' ' + event['summary']
-                    print(event_str)
-                
-                if event_ymd == ymd:
-                    jpredtext(event_str, 5, x, mf, epd)
-                else:
-                    jpblacktext(event_str, 5, x, mf, epd)
-                x = x + 25
-                i = i + 1
+
+        if access_token is None:
+            jpredtext("カレンダーにアクセス出来ません。", 5, 10, mf, epd)
+            jpredtext("トークンを更新してください。", 5, 35, mf, epd)
         else:
-            print("Failed to retrieve events:", response.status_code, response.text)
+
+            calendar_id = "primary"
+
+            ymdhms = jst_today_ymdhms_for_api() 
+            
+            url = "https://www.googleapis.com/calendar/v3/calendars/" + GOOGLE_CALENDAR_ID + "/events?maxResults=6&timeMin=" + ymdhms + "&orderBy=startTime&singleEvents=True&timeZone=JST"
+
+            headers = {
+                "Authorization": "Bearer " + access_token
+            }
+
+            response = urequests.get(url, headers=headers)
+
+            if response.status_code == 200:
+
+                events = response.json().get('items', [])            
+                
+                # 画面表示
+                epd = EPD_2in13_B_V4_Landscape()
+                epd.Clear(0xff, 0xff)
+                epd.imageblack.fill(0xff)
+                epd.imagered.fill(0xff)
+
+
+                mf = mfont()
+                mf.setFontSize(16)    
+
+                x = 10
+                i = 0
+                
+                for event in events:
+                    if 'date' in event['start']:
+                        event_ymd = event['start']['date'][0:4] + event['start']['date'][5:7] + event['start']['date'][8:10]
+                        event_str = event['start']['date'][5:7] + '月' + event['start']['date'][8:10] + '日' + ' ' + event['summary']
+                        print(event_str)
+                    elif 'dateTime' in event['start']:
+                        event_ymd = event['start']['dateTime'][0:4] + event['start']['dateTime'][5:7] + event['start']['dateTime'][8:10]
+                        event_str = event['start']['dateTime'][5:7] + '月' + event['start']['dateTime'][8:10] + '日' +   event['start']['dateTime'][11:16] + ' ' + event['summary']
+                        print(event_str)
+                    
+                    if event_ymd == ymd:
+                        jpredtext(event_str, 5, x, mf, epd)
+                    else:
+                        jpblacktext(event_str, 5, x, mf, epd)
+                    x = x + 25
+                    i = i + 1
+            else:
+                print("Failed to retrieve events:", response.status_code, response.text)
 
 
         epd.display()
